210e24323edbba9f0fb95da07df43aea92c0e12e
hk469348 <hk469348@students.mimuw.edu.pl>
2026-05-12 20:05:30 +0200

    function to parse URL

---
+++ sikradio/url_parser.cpp
@@ -0,0 +1,64 @@
+#include "url_parser.h"
+
+std::optional<ParsedURL> parseUrl(const std::string& url) {
+    ParsedURL result;
+
+    size_t offset = 0;
+
+    // find out which protocol and set default ports
+    if (url.starts_with("http://")) {
+        result.protocol = Protocol::HTTP;
+        result.port_str = DefaultPort::HTTP;
+        offset = ProtocolOffsets::HTTP;
+    } else if (url.starts_with("https://")) {
+        result.protocol = Protocol::HTTPS;
+        result.port_str = DefaultPort::HTTPS;
+        offset = ProtocolOffsets::HTTPS;
+    } else {
+        result.protocol = Protocol::HTTP;
+        result.port_str = DefaultPort::HTTP;
+    }
+
+    // find the path
+    // default: '/'
+    size_t const slash_position = url.find('/', offset);
+    std::string host_buffer;
+
+    if (slash_position != std::string::npos) {
+        // path found
+        result.path = url.substr(slash_position);
+        host_buffer = url.substr(offset, slash_position - offset);
+    } else {
+        // path not found
+        result.path = "/"; // empty path
+        host_buffer = url.substr(offset);
+    }
+
+    // check if there is a unusual port specified (number after the last ':')
+    size_t const colon_position = host_buffer.rfind(':');
+    size_t const bracket_position = host_buffer.rfind(']');
+
+    if (colon_position != std::string::npos &&
+        (bracket_position == std::string::npos || colon_position > bracket_position)) {
+
+        result.port_str = host_buffer.substr(colon_position + 1); // +1 not to include ':'
+        result.hostname = host_buffer.substr(0, colon_position);
+    } else {
+        // no unusual port provided
+        result.hostname = host_buffer;
+    }
+
+    // if it was raw IPv6 address, strip it off the brackets
+    if (!result.hostname.empty() && result.hostname.front() == '[' && result.hostname.back() == ']') {
+        result.hostname = result.hostname.substr(1, result.hostname.size() - 2);
+    }
+
+    // validate if there is even a host
+    if (result.hostname.empty()) {
+        return std::nullopt;
+    }
+
+    return result;
+}
+
+
---
+++ sikradio/url_parser.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+enum class Protocol {
+    HTTP,
+    HTTPS
+};
+
+namespace ProtocolOffsets {
+    constexpr size_t HTTP = 7;
+    constexpr size_t HTTPS = 8;
+};
+
+namespace DefaultPort {
+    constexpr const char *HTTP = "80";
+    constexpr const char *HTTPS = "443";
+}
+
+struct ParsedURL {
+    Protocol protocol;
+    std::string hostname;
+    std::string port_str;
+    std::string path;
+};
+
+std::optional<ParsedURL> parseUrl(const std::string& url);
+
