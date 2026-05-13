d2a20500069316d5239d133851c28dd587881f34
hk469348 <hk469348@students.mimuw.edu.pl>
2026-05-13 14:41:32 +0200

    builiding a http request

--- sikradio/http_logic.cpp
+++ sikradio/http_logic.cpp
@@ -1,5 +1,8 @@
 #include "http_logic.h"
 
+#include <unistd.h>
+#include <iostream>
+
 std::string build_http_request(const ParsedURL& parsed_url, const ClientConfig& config, const std::string& current_cookie) {
     std::string request = "GET " + parsed_url.path + " HTTP/1.1\r\n";
     request += "Host: " + parsed_url.hostname + "\r\n";
@@ -17,4 +20,17 @@ std::string build_http_request(const ParsedURL& parsed_url, const ClientConfig& 
     request += "\r\n";
 
     return request;
+}
+
+void send_http_request(const int socket_fd, const ParsedURL& parsed_url, const ClientConfig& config, const std::string& current_cookie) {
+    std::string request = build_http_request(parsed_url, config, current_cookie);
+
+    ssize_t const bytes_written = write(socket_fd, request.data(), request.length());
+
+    if (bytes_written < 0 || static_cast<size_t>(bytes_written) != request.length()) {
+        throw std::runtime_error("failed to write HTTP request to socket");
+    }
+
+    // TODO: wypisywanie logów w zależnośc
+
 }
--- sikradio/http_logic.h
+++ sikradio/http_logic.h
@@ -5,3 +5,5 @@
 #include "network_logic.h"
 
 std::string build_http_request(const ParsedURL& parsed_url, const ClientConfig& config, const std::string& current_cookie = "");
+
+void sent_http_request(const int socket_fd, const ParsedURL& parsed_url, const ClientConfig& config, const std::string current_cookie = "");
