35fc6ef446168c37dae539827e23041275288dc8
hk469348 <hk469348@students.mimuw.edu.pl>
2026-05-24 19:09:23 +0200

    removed socket timeout configuration as timeout is only for receiving music

--- sikradio/network_logic.cpp
+++ sikradio/network_logic.cpp
@@ -117,30 +117,10 @@ static int connect_to_the_first_working_address(const struct addrinfo* addresses
     return socket_fd;
 }
 
-/**
- * @brief Configures the maximum wait time (timeout) for receiving data from the socket.
- * Converts milliseconds into a timeval structure and applies it using the SO_RCVTIMEO flag.
- */
-static void configure_socket_timeout(const int socket_fd, const uint32_t timeout, const int verbosity) {
-    struct timeval tv = {};
-    tv.tv_sec = timeout / 1000;
-    tv.tv_usec = (timeout % 1000) * 1000;
-
-    if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, reinterpret_cast<const char *>(&tv), sizeof(tv)) == -1) {
-        close(socket_fd);
-        throw std::runtime_error(std::string("failed to set socket receive timeout"));
-    }
-
-    log_message(verbosity, VerbosityLevel::DEBUG, "####DEBUG#### socket timeout set to " + std::to_string(tv.tv_sec) + "." + std::to_string(tv.tv_usec) + " s");
-
-}
-
 std::unique_ptr<IStream> connect_to_server(const ParsedURL& parsed_url, const ClientConfig& config) {
     AddrInfoPtr const resolved_address = resolve_hostname(parsed_url, config);
 
     int const socket_fd = connect_to_the_first_working_address(resolved_address.get(), config.verbosity);
-
-    configure_socket_timeout(socket_fd, config.timeout, config.verbosity);
 
     if (parsed_url.protocol == Protocol::HTTPS) {
         return std::make_unique<TlsStream>(socket_fd, parsed_url.hostname);
